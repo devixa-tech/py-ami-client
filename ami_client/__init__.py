fe80b78b09aa73f1d92ed10a849be35ebbfac2f2
Mohammad Heydari <m_heydari@razi.cc>
2026-06-17 16:01:10 +0330

    updated_mainfest_to_match_org_rules

--- ami_client/__init__.py
+++ ami_client/__init__.py
@@ -1,7 +1,17 @@
 from ._ami_client import AMIClient
-from .__version__ import  get_version
+
+__version__ = "0.2.1"
+__description__ = "Python Asterisk Management Interface Client"
+__authors__ = [
+    {"name": "devixa-dev", "email": "hmohammad2520@gmail.com"},
+]
+__keywords__ = [
+    "python",
+    "library",
+    "asterisk",
+    "ami",
+]
 
 __all__ = [
     'AMIClient',
-    'get_version',
 ]
